885ac3134f40df425b603958086eab3f3429f579
adamrstone <adamrstone@gmail.com>
2023-10-09 21:45:53 -0700

    fixed pipenv and added development mode
    1) Stopped installing from pip, and using pipenv instead.
    
    2) Fixed entrpoint/cmd to launch intended script.
    
    3) Added flag to docker-shell.sh to trigger development mode (shell) vs run script directly (needed for vertex AI)

--- src/model/docker-shell.sh
+++ src/model/docker-shell.sh
@@ -5,16 +5,35 @@ set -e
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../../../secrets/
 
+# Parse the runtime argument (1 for development mode, 0 for production mode)
+DEV_FLAG="$1"
+
+if [[ -z "$DEV_FLAG" ]]; then
+    echo "Usage: $0 <dev_flag> (1 launches shell, and 0 launches microservice)"
+    exit 1
+fi
+
 # Create the network if we don't have it yet
-docker network inspect ccb-preprocessing-network >/dev/null 2>&1 || docker network create ccb-preprocessing-network
+docker network inspect ccb-model-network >/dev/null 2>&1 || docker network create ccb-model-network
 
 # Build the image based on the Dockerfile
-docker build -t preprocessing .
+docker build -t model .
 
-# Run Container
-docker run --rm --name ccb-preprocessing -ti --entrypoint /bin/bash \
--v "$BASE_DIR":/app \
--v "$SECRETS_DIR":/../../../secrets \
--v ~/.gitconfig:/etc/gitconfig \
--e GOOGLE_APPLICATION_CREDENTIALS=/../../../secrets/ccb.json \
---network ccb-preprocessing-network preprocessing
+# Run the container with different options based on the development flag
+if [[ "$DEV_FLAG" -eq 1 ]]; then
+    # Development mode: Run with an interactive bash shell
+    docker run --rm --name ccb-model -ti --entrypoint /bin/bash \
+    -v "$BASE_DIR":/app \
+    -v "$SECRETS_DIR":/../../../secrets \
+    -v ~/.gitconfig:/etc/gitconfig \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/../../../secrets/ccb.json \
+    --network ccb-model-network model
+else
+    # Production mode: Run using entrypoint and cmd in dockerfile
+    docker run --rm --name ccb-model \
+    -v "$BASE_DIR":/app \
+    -v "$SECRETS_DIR":/../../../secrets \
+    -v ~/.gitconfig:/etc/gitconfig \
+    -e GOOGLE_APPLICATION_CREDENTIALS=/../../../secrets/ccb.json \
+    --network ccb-model-network model
+fi
