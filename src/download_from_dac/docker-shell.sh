385275f2763dce01e92212e8cfab707a586b923f
Jessica Gochioco <jessica.gochioco@gmail.com>
2023-10-15 14:39:34 -0700

    Fixing some things in docker-shell.sh of download_from_dac

--- src/download_from_dac/docker-shell.sh
+++ src/download_from_dac/docker-shell.sh
@@ -6,15 +6,15 @@ export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/secrets/
 
 # Create the network if we don't have it yet
-docker network inspect ccb-preprocessing-network >/dev/null 2>&1 || docker network create ccb-preprocessing-network
+docker network inspect ccb-download_from_dac-network >/dev/null 2>&1 || docker network create ccb-download_from_dac-network
 
 # Build the image based on the Dockerfile
-docker build -t preprocessing .
+docker build -t download_from_dac .
 
 # Run Container
-docker run --rm --name ccb-preprocessing -ti --entrypoint /bin/bash \
+docker run --rm --name ccb-download_from_dac -ti --entrypoint /bin/bash \
 -v "$BASE_DIR":/app \
 -v "$SECRETS_DIR":/secrets \
 -v ~/.gitconfig:/etc/gitconfig \
 -e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ccb.json
---network ccb-preprocessing-network preprocessing
+--network ccb-download_from_dac-network download_from_dac
