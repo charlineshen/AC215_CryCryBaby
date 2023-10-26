40b53582e16c09ddc3c2119bd7106e7d5220fb60
Jingwen Zhang <jingwen_zhang@g.harvard.edu>
2023-10-26 00:20:39 -0400

    1. updated model container to use scikit learn package. 2. create model1 container to add cry vs. non-cry binary classification.

--- src/model1/model1.py
+++ src/model1/model1.py
@@ -18,7 +18,6 @@ gcp_project = "ac215-project"
 bucket_name = "baby-cry-bucket"
 output_folder = "output_model"
 input_folder = "input_spectrogram"
-labels = ["belly_pain", "burping", "discomfort", "hungry", "tired"]
 
 
 def makedirs():
@@ -38,7 +37,7 @@ def download():
     blobs = bucket.list_blobs(prefix=folder_name)
 
     for blob in blobs:
-        if blob.name.endswith(".npy"):
+        if blob.name.endswith("_model1.npy"):
             print(blob.name)
             blob.download_to_filename(
                 "/app/" + input_folder + "/" + blob.name[len(folder_name) :]
@@ -48,77 +47,66 @@ def download():
 
 
 def load_data():
-    X = np.load("/app/" + input_folder + "/" + "X.npy")
-    y = np.load("/app/" + input_folder + "/" + "y.npy")
+    X = np.load("/app/" + input_folder + "/" + "X_model1.npy")
+    y = np.load("/app/" + input_folder + "/" + "y_model1.npy")
 
     return X, y
 
-def model2(X, y):
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=215)
-    def shuffle_split_data(X, y):
-        np.random.seed(103)
-        arr_rand = np.random.rand(X.shape[0])
-        split = arr_rand < np.percentile(arr_rand, 80)
+def model1(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=215)
+    # def shuffle_split_data(X, y):
+    #     np.random.seed(103)
+    #     arr_rand = np.random.rand(X.shape[0])
+    #     split = arr_rand < np.percentile(arr_rand, 80)
 
-        X_train = X[split]
-        y_train = y[split]
-        X_test = X[~split]
-        y_test = y[~split]
+    #     X_train = X[split]
+    #     y_train = y[split]
+    #     X_test = X[~split]
+    #     y_test = y[~split]
 
-        return X_train, X_test, y_train, y_test
-
-    X_train, X_test, y_train, y_test = shuffle_split_data(X, y)
+    #     return X_train, X_test, y_train, y_test
+    # X_train, X_test, y_train, y_test = shuffle_split_data(X, y)
 
     # Create the model
     num_classes = len(np.unique(y_train))
+    x_shape = X[0].shape
 
-    model = models.Sequential(
-        [
-            layers.Input(
-                shape=(128, 64)
-            ),  # Adjust the input shape based on your spectrogram size
-            layers.Reshape(target_shape=(128, 64, 1)),  # Add a channel dimension
-            layers.Conv2D(32, (3, 3), activation="relu"),
-            layers.MaxPooling2D((2, 2)),
-            layers.Flatten(),
-            layers.Dense(128, activation="relu"),
-            layers.Dense(num_classes, activation="softmax"),
-        ]
-    )
+    model = models.Sequential([
+        layers.Input(shape=(x_shape[0], x_shape[1])),  # Adjust the input shape based on your spectrogram size
+        layers.Reshape(target_shape=(x_shape[0], x_shape[1], 1)),  # Add a channel dimension
+        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(x_shape[0], activation='relu'),
+        layers.Dense(num_classes, activation='softmax') #sigmoid
+    ])
 
     # Compile the model
-    model.compile(
-        optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-    )
+    model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
 
     # Convert labels to one-hot encoding (assuming you have more than one class)
     y_train_encoded = tf.keras.utils.to_categorical(y_train, num_classes)
     y_test_encoded = tf.keras.utils.to_categorical(y_test, num_classes)
-
-    # Use TF data
-    train_data = tf.data.Dataset.from_tensor_slices((X_train, y_train_encoded))
-    validation_data = tf.data.Dataset.from_tensor_slices((X_test, y_test_encoded))
+    
 
     # Train the model
-    train_batch_size = 32
-    val_batch_size = 16
-    epochs = 1  # todo change to 10
+    batch_size = 32
+    epochs = 10
 
-    history = model.fit(
-        train_data.batch(train_batch_size),
-        epochs=epochs,
-        validation_data=validation_data.batch(val_batch_size),
-    )
+    history = model.fit(X_train, y_train_encoded, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
 
     # Evaluate the model on the test set
-    # y_pred = model.predict(X_test)
-    # y_pred_classes = np.argmax(y_pred, axis=1)
-    # y_test_classes = np.argmax(y_test_encoded, axis=1)
+    y_pred = model.predict(X_test)
+    y_pred_classes = np.argmax(y_pred, axis=1)
+    y_test_classes = np.argmax(y_test_encoded, axis=1)
 
-    # accuracy = accuracy_score(y_test_classes, y_pred_classes)
-    # print(f'Test accuracy: {accuracy:.2f}')
+    accuracy = accuracy_score(y_test_classes, y_pred_classes)
+    print(f'Test accuracy: {accuracy:.2f}')
 
-    model.save("/app/" + output_folder + "/" + "model2_v1.h5")
+    model.save("/app/" + output_folder + "/" + "model1_v1.h5")
 
     return
 
@@ -151,7 +139,7 @@ def main(args=None):
     makedirs()
     download()
     X, y = load_data()
-    model2(X, y)
+    model1(X, y)
     upload()
 
 
