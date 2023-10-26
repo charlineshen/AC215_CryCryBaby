43415393f6a78d51fe7f4c2f9efbac70d139300e
adamrstone <adamrstone@gmail.com>
2023-10-26 01:54:29 -0700

    vertex ai pipeline functioning
    pipeline: download_from_dac -> preprocessing -> model1.
    
    need to manually upload pipeline json to vertex ai pipeline.

---
+++ src/preprocessing/preprocessing-docker-shell-vertex-ai.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Create the network if we don't have it yet
+docker network inspect ccb-preprocessing-network >/dev/null 2>&1 || docker network create ccb-preprocessing-network
+
+# Get the latest commit hash
+
+# Build and tag the Docker image with the commit hash
+docker build -t us-east1-docker.pkg.dev/ac215-project-400018/ac215-crycrybaby-ar/preprocessing:latest .
+
+# Push the image to the registry
+docker push us-east1-docker.pkg.dev/ac215-project-400018/ac215-crycrybaby-ar/preprocessing:latest
