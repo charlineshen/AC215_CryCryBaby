e2c5503495b472ba05642c71962b984f7bd10793
adamrstone <adamrstone@gmail.com>
2023-11-19 23:38:20 -0800

    Update Google Application Credentials path in docker-shell.sh and add debug logs in Dockerfile

--- src/api-service/docker-shell.sh
+++ src/api-service/docker-shell.sh
@@ -22,6 +22,6 @@ docker run --rm --name $IMAGE_NAME -ti \
 -v "$PERSISTENT_DIR":/persistent \
 -p 9000:9000 \
 -e DEV=1 \
--e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ml-workflow.json \
+-e GOOGLE_APPLICATION_CREDENTIALS=/secrets/ccb.json \
 -e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
 $IMAGE_NAME
