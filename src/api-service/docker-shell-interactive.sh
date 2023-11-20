2fffd599bede11a2a442e306d0d154edb96ac821
adamrstone <adamrstone@gmail.com>
2023-11-20 15:08:41 -0800

    Add Dockerfile and update docker shell script

--- src/api-service/docker-shell-interactive.sh
+++ src/api-service/docker-shell-interactive.sh
@@ -6,5 +6,9 @@
 # Then push the updated Pipfile.lock to git
 export IMAGE_NAME="src-frontend-react"
 
+docker build -t $IMAGE_NAME -f Dockerfile .
+echo "Finished building docker image"
+echo "Running docker container"
+
 docker run --rm --name $IMAGE_NAME -ti -p 9000:9000 --entrypoint /bin/bash\
     -v $(pwd):/app $IMAGE_NAME \
