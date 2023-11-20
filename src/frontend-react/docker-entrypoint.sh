d04df1e4dd19c334aace67eb1799cfa371af8298
adamrstone <adamrstone@gmail.com>
2023-11-20 01:58:24 -0800

    Update Dockerfile and docker-shell-prod.sh

--- src/frontend-react/docker-entrypoint.sh
+++ src/frontend-react/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "frontend-react container is running"
 
--- src/frontend-react/docker-shell-prod.sh
+++ src/frontend-react/docker-shell-prod.sh
@@ -5,4 +5,6 @@ set -e
 export IMAGE_NAME="frontend-react"
 
 docker build -t $IMAGE_NAME -f Dockerfile .
-docker run --rm --name $IMAGE_NAME -ti -p 80:80 --entrypoint /bin/bash $IMAGE_NAME
+echo "Finished building docker image"
+echo "Running docker container"
+docker run --rm --name $IMAGE_NAME -ti -p 80:80 --entrypoint /bin/sh $IMAGE_NAME
