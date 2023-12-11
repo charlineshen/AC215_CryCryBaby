0a6ab1868f5174ac805d2c3b92659d5d6cb464c4
Tingwei Shen <tingweishen@berkeley.edu>
2023-12-10 23:45:44 -0500

    /run-deploy-app test

--- src/deployment/deploy-app.sh
+++ src/deployment/deploy-app.sh
@@ -1,2 +1,6 @@
+echo "Testing..."
+
 ansible-playbook deploy-docker-images.yml -i inventory.yml -vvv
+
+echo "Finishing deploy docker images"
 # ansible-playbook update-k8s-cluster.yml -i inventory-prod.yml -vvv
